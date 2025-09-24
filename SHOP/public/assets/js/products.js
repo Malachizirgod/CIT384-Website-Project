e89dec84cfa10f3e71525702e1803b396b862e31
Malachizirgod <kuirbg@gmail.com>
2025-09-23 19:23:43 -0700

    09/23

--- SHOP/public/assets/js/products.js
+++ SHOP/public/assets/js/products.js
@@ -5,4 +5,8 @@ window.CATALOG = [
 { id: 'cap', name: 'Embroidered Cap', price: 22.00, img: 'assets/img/cap.jpg' },
 { id: 'sticker-pack', name: 'Sticker Pack (5)', price: 6.00, img: 'assets/img/stickers.jpg' },
 { id: 'gloves', name: 'Gloves', price: 9.50, img: 'assets/img/gloves.jpg' },
+{ id:'tee-classic', name:'Classic Chapter Tee', price:18.00,
+  img:'assets/img/tee_front.jpg',
+  imgs:['assets/img/tee_front.jpg','assets/img/tee_back.jpg'],
+  desc:'Soft cotton tee with chapter logo. Unisex sizing S–XXL.' }
 ];
